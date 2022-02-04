64c615219ecd68e765272ba2c13889e68b8c4dea
Mikko Rajakorpi <mikko@pop-os.localdomain>
2022-02-04 23:07:38 +0200

    Beginnings of content and css changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+const Content = () => {
+  return (
+    <div class="content">
+      <h2>Welcome to Mikko Rajakorpi's personal homepage</h2>
+      <p>This is still very wip</p>
+    </div>
+  )
+}
+
+export default Content
